a4010541a7624c82e6a213e628af1f5df3fc24db
Sagah123 <globa7002@gmail.com>
2026-04-27 10:18:18 +0200

    Few operations realized

---
+++ CHIP-8.h
@@ -0,0 +1,20 @@
+#include <cstdint>
+#include <string>
+#include <array>
+
+
+class Chip8 {
+    public:
+        std::array<uint8_t, 16> V{};
+        std::array<uint8_t, 4096> memory{};
+        uint16_t I{};
+        uint16_t pc = 0x200;
+        std::array<std::array<bool,64>, 32> display{};
+        std::array<uint16_t, 16> stack{};
+        uint8_t sp{};
+        uint8_t delay_timer{};
+        uint8_t sound_timer{};
+        Chip8();
+        bool loadROM(const std::string& filename);
+        void emulateCycle();
+};
---
+++ Chip-8.cpp
@@ -0,0 +1,191 @@
+#include "CHIP-8.h"
+#include <iostream>
+#include <fstream>
+
+Chip8::Chip8() {
+    std::array<uint8_t, 80> fontset = {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // Number 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // Number 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // Number 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // Number 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // Number 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // Number 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // Number 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // Number 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // Number 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // Number 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // Letter A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // Letter B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // Letter C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // Letter D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // Letter E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // Letter F
+    };
+    for (int i = 0; i < 80; i++){
+        memory[0x050+i] = fontset[i];
+    }
+}
+
+bool Chip8::loadROM(const std::string& filename){
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open ROM with name: " << filename << std::endl;
+        return false;
+    
+    }
+    auto fileSize = file.tellg();
+    if (fileSize > (Chip8::memory.size() - 0x200)) {
+        std::cerr << "ROM size exceeds available memory." << std::endl;
+        return false;
+    }
+    file.seekg(0);  
+    file.read(reinterpret_cast<char*>(&memory[pc]), fileSize);
+    file.close();
+    return true;
+}
+
+void Chip8::emulateCycle(){
+    // ==========================================
+    // 1. FETCH
+    // ==========================================
+    uint16_t command = (memory[pc] << 8) | memory[pc+1];
+    pc += 2;
+
+    // ==========================================
+    // 2. DISMEMBER
+    // ==========================================
+    uint8_t prefix = (command & 0xF000) >> 12;
+    uint8_t x = (command & 0x0F00) >> 8;
+    uint8_t y = (command & 0x00F0) >> 4;
+    uint8_t N = (command & 0x000F);
+    uint16_t NN = (command & 0x00FF);
+    uint16_t NNN = (command & 0x0FFF);
+
+    // ==========================================
+    // 3. EXECUTE
+    // ==========================================
+    switch (prefix)
+    {
+    case 0x0:
+        //clear or return from subroutine
+        switch (NNN)
+        {
+        case 0x0EE:
+            //return from subroutine call
+            sp--;
+            pc = stack[sp];
+            break;
+        case 0x0E0:
+            //clear the screen
+            for (auto& row: display){
+                for (auto& pixel: row){
+                    pixel = 0;
+                }
+            }
+        }
+
+        
+        break;
+    case 0x1:
+        //jump to NNN
+        pc = NNN;
+        break;
+    case 0x2:
+        //jump a subroutine
+        stack[sp] = pc;
+        sp++;
+        pc = NNN;
+        break;
+    case 0x3:
+        //skip if vx equals NN
+        if (V[x] == NN) pc +=2;
+        break;
+    case 0x4:
+        //skip if vx not equals NN 
+        if (V[x] != NN) pc +=2;
+        break;
+    case 0x5:
+        //skip if vx equals vy
+        if (V[x] == V[y]) pc+=2;
+        break;
+    case 0x6:
+        //move constant to register vx
+        V[x] = NN;
+        break;
+    case 0x7:
+        //add constant to register vx (No carry:( )
+        V[x] += NN;
+        break;
+    case 0x8:
+        //Suka pizdec dolbaebi ponapishut huini svoei
+        switch (N)
+        {
+        case 0x1:
+            //or register vy into register vx
+            V[x] |= V[y];
+            break;
+        case 0x2:
+            //and register vy into register vx
+            V[x] &= V[y];
+            break;
+        case 0x3:
+            //xor register vy into register vx
+            V[x] ^= V[y];
+            break;
+        case 0x4:
+            //add register vy into register vx, carry in vf
+            
+            break;
+        case 0x5:
+            //substract register vy from register vx, borrow in vf
+            
+            break;
+        case 0x6:
+            //shift register vy right, bit 0 goes into register vf
+            
+            break;
+        case 0x7:
+            //substract register vx from register vy, borrow in vf (reversed 0x5 operation)
+            
+            break;
+        case 0xe:
+            //shift register vr left,bit 7 goes into register vf
+            
+            break;  
+        }
+        
+        break;
+    case 0x9:
+        //skip if vx not equals vy
+        if (V[x] != V[y]) pc+=2;
+        break;
+    case 0xa:
+        //load index register with NNN
+        I = NNN;
+        break;
+    case 0xb:
+        //Jump to address NNN+register
+        
+        break;
+    case 0xc:
+        //random number less than or equal to NNN
+        
+        break;
+    case 0xd:
+        //Draw sprite at screen location rx,ry height s (dorabotka ebat)
+
+        break;
+    case 0xe:
+        //Skip if key chtoto tam blyat
+
+        break;
+    case 0xf:
+        //tam ebat funcktsiy suka
+        
+        break;
+    default:
+        //Undefined instruction
+        break;
+    }
+
+};
